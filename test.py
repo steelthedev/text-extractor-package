fc7c12add1c1aafd6d45a60334dc933bdc6d2544
Akinwumi Iyanuoluwa Ayomiposi <akinwumikaliyanu@gmail.com>
2022-09-12 19:19:53 +0100

    added requirements

--- test.py
+++
@@ -1,9 +0,0 @@
-from ext import ImgExt
-
-image_text = ImgExt(image_path="img/diabetes.png", file_name="my_new_package2")
-  
-
-pk=image_text.get_text()
-
-p = image_text.convert_to_word()
-print(p)
