e3680671a383abbaee75d93f17382e9e3457b964
Akinwumi Iyanuoluwa Ayomiposi <akinwumikaliyanu@gmail.com>
2022-09-14 07:38:45 +0100

    updated license

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 import setuptools
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="extract-img-txt",
-    version="1.0.0",
+    version="1.0.1",
     description="A simple text extractor from image",
     author="Iyanuoluwa Akinwumi",
     author_email="akinwumikaliyanu@gmail.com",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
     packages=["image_extract"],
-    license="LICENSE.txt",
+    license=":: MIT License",
+    project_urls ="https://github.com/steelthedev/text-extractor-package",
     install_requires=[                                                                                                              
         'pytesseract',
         'python-docx',
